97b51876bd7c813be4440aa968caa70d658b29cc
Angel <lopeztomaylla1299@gmail>
2020-07-17 19:13:47 -0500

    delman: agrego el activity de control

--- app/src/main/java/proyecto/pidetucomida/actividades/Controles.java
+++ app/src/main/java/proyecto/pidetucomida/actividades/Controles.java
@@ -3,14 +3,20 @@ package proyecto.pidetucomida.actividades;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import proyecto.pidetucomida.R;
 
 public class Controles extends AppCompatActivity {
+    Button btnListar,btnBuscar,btnActualizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controles);
+        btnListar= findViewById(R.id.btnListar);
+        btnBuscar= findViewById(R.id.btnBuscar);
+        btnActualizar= findViewById(R.id.btnActualizar);
+
     }
 }
